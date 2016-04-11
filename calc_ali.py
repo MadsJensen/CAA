dbb379823627a26f14bc015f135d72e5be3d84b1
Mads Jensen <mje.mads@gmail.com>
2016-04-11 10:44:41 +0200

    Function to calc ALI

---
+++ calc_ali.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from my_settings import *
+
+ctl_left_roi_left_cue = np.load(tf_folder +
+                                "%s_pow_ctl_left_Brodmann.17-lh.npy"
+                                % (subject))
+ctl_right_roi_left_cue = np.load(tf_folder +
+                                 "%s_pow_ctl_left_Brodmann.17-rh.npy"
+                                 % (subject))
+ctl_left_roi_right_cue = np.load(tf_folder +
+                                 "%s_pow_ctl_right_Brodmann.17-lh.npy"
+                                 % (subject))
+ctl_right_roi_right_cue = (tf_folder +
+                           "%s_pow_ctl_right_Brodmann.17-rh.npy"
+                           % (subject))
+
+ALI_left_cue_ctl = ((ctl_left_roi_left_cue.data.mean(axis=0) -
+                     ctl_right_roi_left_cue.data.mean(axis=0)) /
+                    (ctl_left_roi_left_cue.data.mean(axis=0) +
+                     ctl_right_roi_left_cue.data.mean(axis=0)))
+
+ALI_right_cue_ctl = ((ctl_left_roi_right_cue.data.mean(axis=0) -
+                      ctl_right_roi_right_cue.data.mean(axis=0)) /
+                     (ctl_left_roi_right_cue.data.mean(axis=0) +
+                      ctl_right_roi_right_cue.data.mean(axis=0)))
