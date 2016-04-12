75ecba43cf91d5c692524db4d429fb3cbf27b557
Mads Jensen <mje.mads@gmail.com>
2016-04-12 20:18:43 +0200

    Made to a function

--- calc_ali.py
+++ calc_ali.py
@@ -2,25 +2,45 @@ import numpy as np
 
 from my_settings import *
 
-ctl_left_roi_left_cue = np.load(tf_folder +
-                                "%s_pow_ctl_left_Brodmann.17-lh.npy"
-                                % (subject))
-ctl_right_roi_left_cue = np.load(tf_folder +
-                                 "%s_pow_ctl_left_Brodmann.17-rh.npy"
-                                 % (subject))
-ctl_left_roi_right_cue = np.load(tf_folder +
-                                 "%s_pow_ctl_right_Brodmann.17-lh.npy"
-                                 % (subject))
-ctl_right_roi_right_cue = np.load(tf_folder +
-                                  "%s_pow_ctl_right_Brodmann.17-rh.npy"
-                                  % (subject))
 
-ALI_left_cue_ctl = ((ctl_left_roi_left_cue.mean(axis=0).mean(axis=0) -
-                     ctl_right_roi_left_cue.mean(axis=0).mean(axis=0)) /
-                    (ctl_left_roi_left_cue.mean(axis=0).mean(axis=0) +
-                     ctl_right_roi_left_cue.mean(axis=0).mean(axis=0)))
+def calc_ali(subject):
+    """
+    Keyword Arguments:
+    subject : the subject number
+    """
 
-ALI_right_cue_ctl = ((ctl_left_roi_right_cue.mean(axis=0).mean(axis=0) -
-                      ctl_right_roi_right_cue.mean(axis=0).mean(axis=0)) /
-                     (ctl_left_roi_right_cue.mean(axis=0).mean(axis=0) +
-                      ctl_right_roi_right_cue.mean(axis=0).mean(axis=0)))
+    ctl_left_roi_left_cue = np.load(tf_folder +
+                                    "%s_pow_ctl_left_Brodmann.17-lh.npy"
+                                    % (subject))
+    ctl_right_roi_left_cue = np.load(tf_folder +
+                                     "%s_pow_ctl_left_Brodmann.17-rh.npy"
+                                     % (subject))
+    ctl_left_roi_right_cue = np.load(tf_folder +
+                                     "%s_pow_ctl_right_Brodmann.17-lh.npy"
+                                     % (subject))
+    ctl_right_roi_right_cue = np.load(tf_folder +
+                                      "%s_pow_ctl_right_Brodmann.17-rh.npy"
+                                      % (subject))
+
+    ali_left_cue_ctl = ((ctl_left_roi_left_cue.mean(axis=0).mean(axis=0) -
+                         ctl_right_roi_left_cue.mean(axis=0).mean(axis=0)) /
+                        (ctl_left_roi_left_cue.mean(axis=0).mean(axis=0) +
+                         ctl_right_roi_left_cue.mean(axis=0).mean(axis=0)))
+
+    ali_right_cue_ctl = ((ctl_left_roi_right_cue.mean(axis=0).mean(axis=0) -
+                          ctl_right_roi_right_cue.mean(axis=0).mean(axis=0)) /
+                         (ctl_left_roi_right_cue.mean(axis=0).mean(axis=0) +
+                          ctl_right_roi_right_cue.mean(axis=0).mean(axis=0)))
+
+    ali_left_cue_ent = ((ent_left_roi_left_cue.mean(axis=0).mean(axis=0) -
+                         ent_right_roi_left_cue.mean(axis=0).mean(axis=0)) /
+                        (ent_left_roi_left_cue.mean(axis=0).mean(axis=0) +
+                         ent_right_roi_left_cue.mean(axis=0).mean(axis=0)))
+
+    ali_right_cue_ent = ((ent_left_roi_right_cue.mean(axis=0).mean(axis=0) -
+                          ent_right_roi_right_cue.mean(axis=0).mean(axis=0)) /
+                         (ent_left_roi_right_cue.mean(axis=0).mean(axis=0) +
+                          ent_right_roi_right_cue.mean(axis=0).mean(axis=0)))
+
+    return (ali_left_cue_ctl, ali_right_cue_ctl,
+            ali_left_cue_ent, ali_right_cue_ent)
