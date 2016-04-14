b4f2f3f5373ec780f39f6858949a7d0a229ae9b0
Mads Jensen <mje.mads@gmail.com>
2016-04-14 09:01:42 +0200

    added ent and method param

--- calc_ali.py
+++ calc_ali.py
@@ -3,24 +3,40 @@ import numpy as np
 from my_settings import *
 
 
-def calc_ali(subject):
+def calc_ali(subject, method="dSPM"):
     """
-    Keyword Arguments:
+    Params
+    ------
     subject : the subject number
+    method : str
+        The method used for inverse model.
     """
 
     ctl_left_roi_left_cue = np.load(tf_folder +
-                                    "%s_pow_ctl_left_Brodmann.17-lh.npy"
-                                    % (subject))
+                                    "%s_pow_ctl_left_%s_Brodmann.17-lh.npy"
+                                    % (subject, method))
     ctl_right_roi_left_cue = np.load(tf_folder +
-                                     "%s_pow_ctl_left_Brodmann.17-rh.npy"
-                                     % (subject))
+                                     "%s_pow_ctl_left_%s_Brodmann.17-rh.npy"
+                                     % (subject, method))
     ctl_left_roi_right_cue = np.load(tf_folder +
-                                     "%s_pow_ctl_right_Brodmann.17-lh.npy"
-                                     % (subject))
+                                     "%s_pow_ctl_right_%s_Brodmann.17-lh.npy"
+                                     % (subject, method))
     ctl_right_roi_right_cue = np.load(tf_folder +
-                                      "%s_pow_ctl_right_Brodmann.17-rh.npy"
-                                      % (subject))
+                                      "%s_pow_ctl_right_%s_Brodmann.17-rh.npy"
+                                      % (subject, method))
+
+    ent_left_roi_left_cue = np.load(tf_folder +
+                                    "%s_pow_ent_left_%s_Brodmann.17-lh.npy"
+                                    % (subject, method))
+    ent_right_roi_left_cue = np.load(tf_folder +
+                                     "%s_pow_ent_left_%s_Brodmann.17-rh.npy"
+                                     % (subject, method))
+    ent_left_roi_right_cue = np.load(tf_folder +
+                                     "%s_pow_ent_right_%s_Brodmann.17-lh.npy"
+                                     % (subject, method))
+    ent_right_roi_right_cue = np.load(tf_folder +
+                                      "%s_pow_ent_right_%s_Brodmann.17-rh.npy"
+                                      % (subject, method))
 
     ali_left_cue_ctl = ((ctl_left_roi_left_cue.mean(axis=0).mean(axis=0) -
                          ctl_right_roi_left_cue.mean(axis=0).mean(axis=0)) /
