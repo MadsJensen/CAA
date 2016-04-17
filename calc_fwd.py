ffcef3294dc7364df92306415ae9b9e5df95cf0e
Mads Jensen <mje.mads@gmail.com>
2016-04-17 18:44:06 +0200

    Script to calculate forward model

---
+++ calc_fwd.py
@@ -0,0 +1,40 @@
+"""
+Doc string goes here.
+
+@author: mje mads [] cnru.dk
+"""
+
+
+import socket
+import mne
+from mne.minimum_norm import make_inverse_operator
+import os
+# import subprocess
+import sys
+import glob 
+
+from my_settings import *
+
+subject = sys.argv[1]
+
+bem_list = glob.glob("%s-8192-8192*sol.fif" % subject)
+
+raw_fname = save_folder + "%s_filtered_ica_mc_raw_tsss.fif" % subject
+trans_fname = mne_folder + "%s-trans.fif" % subject
+cov = mne.read_cov(mne_folder + "%s-cov.fif" % subject)
+bem = mne_folder + bem_list[0]
+
+src = mne.setup_source_space(subject,
+                             mne_folder + "%s-all-src.fif" % subject,
+                             spacing="all",
+                             subjects_dir=subjects_dir,
+                             n_jobs=2,
+                             overwrite=True)  # 1 for each hemispere
+
+fwd = mne.make_forward_solution(raw_fname, trans=trans_fname,
+                                src=src,
+                                bem=bem,
+                                meg=True,
+                                eeg=True,
+                                fname=mne_folder + "%s-fwd.fif" % subject,
+                                overwrite=True)
