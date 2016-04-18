228ebd31aeef41b2073c28b737235835c148bec7
Mads Jensen <mje.mads@gmail.com>
2016-04-18 14:01:00 +0200

    clean up

--- calc_cov.py
+++ calc_cov.py
@@ -3,19 +3,11 @@ import sys
 
 from mne import compute_covariance
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
 from my_settings import *
 
 subject = sys.argv[1]
 
 epochs = mne.read_epochs(epochs_folder + "%s_trial_start-epo.fif" % subject)
-epochs.drop_bad_epochs(reject=reject_params)
-
-fig = epochs.plot_drop_log(subject=subject, show=False)
-fig.savefig(epochs_folder + "pics/%s_drop_log.png" % subject)
 
 # Make noise cov
 cov = compute_covariance(epochs, tmin=None, tmax=-0.2,
