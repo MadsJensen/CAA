f94daddc53bde421c3f08309eafa9b025ae6d103
Mads Jensen <mje.mads@gmail.com>
2016-04-18 07:24:23 +0200

    Use source space

--- calc_fwd.py
+++ calc_fwd.py
@@ -22,13 +22,14 @@ raw_fname = save_folder + "%s_filtered_ica_mc_raw_tsss.fif" % subject
 trans_fname = mne_folder + "%s-trans.fif" % subject
 cov = mne.read_cov(mne_folder + "%s-cov.fif" % subject)
 bem = glob.glob(mne_folder + "%s-8192-8192*sol.fif" % subject)[0]
+src = subjects_dir + "%s/bem/%s-oct-6-src.fif" % (subject, subject)
 
-src = mne.setup_source_space(subject,
-                             mne_folder + "%s-all-src.fif" % subject,
-                             spacing="all",
-                             subjects_dir=subjects_dir,
-                             n_jobs=1,
-                             overwrite=True)  # 1 for each hemispere
+# src = mne.setup_source_space(subject,
+#                              mne_folder + "%s-all-src.fif" % subject,
+#                              spacing="all",
+#                              subjects_dir=subjects_dir,
+#                              n_jobs=1,
+#                              overwrite=True)  # 1 for each hemispere
 
 fwd = mne.make_forward_solution(raw_fname, trans=trans_fname,
                                 src=src,
