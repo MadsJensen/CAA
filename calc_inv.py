ca9c1941e65ebbe9fec2f40e6fdda7a8c2d15f28
Mads Jensen <mje.mads@gmail.com>
2016-04-21 16:22:48 +0200

    added surf_ori to read fwd

--- calc_inv.py
+++ calc_inv.py
@@ -5,12 +5,13 @@ from mne.minimum_norm import make_inverse_operator
 
 subject = sys.argv[1]
 
-fwd = mne.read_forward_solution(mne_folder + "%s-fwd.fif" % subject)
+fwd = mne.read_forward_solution(mne_folder + "%s-fwd.fif" % subject,
+                                surf_ori=True)
 cov = mne.read_cov(mne_folder + "%s-cov.fif" % subject)
 epochs = mne.read_epochs(epochs_folder +
                          "%s_trial_start-epo.fif" % subject,
                          preload=False)
-epochs.drop_bad_epochs(reject_params)
+
 
 inv = make_inverse_operator(epochs.info, fwd, cov,
                             loose=0.2, depth=0.8)
