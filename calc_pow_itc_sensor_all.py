dd32262ed27deb4646ff80b82c851ff87a3f2858
Mads Jensen <mje.mads@gmail.com>
2016-04-22 17:04:00 +0200

    initail commit

---
+++ calc_pow_itc_sensor_all.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 16:55:00 2016
+
+@author: mje
+"""
+
+import mne
+import numpy as np
+from my_settings import *
+
+freqs = np.arange(8, 13, 1)  # define frequencies of interest
+n_cycles = freqs / 2.  # different number of cycle per frequency
+
+sides = ["left", "right"]
+conditions = ["ctl", "ent"]
+
+for subject in subjects_select:
+    epochs = mne.read_epochs(epochs_folder +
+                         "%s_trial_start-epo.fif" % subject,
+                         preload=False)
+    for cond in conditions:
+        for side in sides:
+            power, itc = tfr_morlet(epochs[cond + "/" + side], freqs=freqs,
+                                    n_cycles=n_cycles,
+                                    use_fft=True, return_itc=True, decim=2,
+                                    n_jobs=1)
+            power.save(tf_folder + "%s_%s_%s-tfr.h5" % (subject,
+                                                        condition, side),
+                                                        overwrite=True)
+            itc.save(tf_folder + "%s_%s_%s-tfr.h5" % (subject,
+                                                      condition, side),
+                                                      overwrite=True)
