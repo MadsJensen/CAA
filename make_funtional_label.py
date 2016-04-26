b3b7d49013bcc90e248f2fa861e2664eb781a4c5
Mads Jensen <mje.mads@gmail.com>
2016-04-26 22:09:26 +0200

    Using lobe to generate labels from

--- make_funtional_label.py
+++ make_funtional_label.py
@@ -1,7 +1,6 @@
 from my_settings import *
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 import mne
 from mne.minimum_norm import read_inverse_operator, apply_inverse
@@ -14,43 +13,32 @@ method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
 
 # Compute a label/ROI based on the peak power between 80 and 120 ms.
 # The label bankssts-lh is used for the comparison.
-tmin, tmax = 0.1, 0.7
+tmin, tmax = 0., 0.7
 
 subject = sys.argv[1]
 
-#def make_functional_label(subject, tmin=0.1, tmax=0.7, save=True):
-#    """
-#    Params
-#    ------
-#    epochs : Epochs
-#        The epochs to use for the functional label.
-#    tmin : float
-#        start time.
-#    tmax : float
-#        end time.
-#    save : bool
-#        To save the label or not.
-#
-#    """
-
 # Load data
-epochs = mne.read_epochs(epochs_folder + "%s_trial_start-epo.fif" 
+epochs = mne.read_epochs(epochs_folder + "%s_trial_start-epo.fif"
                          % subject)
 epochs.drop_bad_epochs(reject_params)
 
-inv = mne.minimum_norm.read_inverse_operator(mne_folder + "%s-inv.fif" 
-                                             % subject)
+inv = read_inverse_operator(mne_folder + "%s-inv.fif"
+                            % subject)
 
 sides = ["left", "right"]
 for side in sides:
     evoked = epochs[side].average()
     src = inv['src']  # get the source space
 
-    labels = mne.read_labels_from_annot(subject,
-                                        parc='PALS_B12_Brodmann',
-                                            regexp="Bro",
+    # labels = mne.read_labels_from_annot(subject,
+    #                                     parc='PALS_B12_Brodmann',
+    #                                         regexp="Bro",
+    #                                     subjects_dir=subjects_dir)
+    # label_lh, label_rh = labels[6], labels[7]
+    labels = mne.read_labels_from_annot(subject, parc='PALS_B12_Lobes',
+                                        # regexp="Bro",
                                         subjects_dir=subjects_dir)
-    label_lh, label_rh = labels[6], labels[7]
+    label_lh, label_rh = labels[9], labels[10]
 
     # Compute inverse solution
     stc = apply_inverse(evoked, inv, lambda2, method,
@@ -81,18 +69,17 @@ for side in sides:
     data = np.abs(stc_mean_label.data)
     stc_mean_label.data[data < 0.6 * np.max(data)] = 0.
 
-    _ , func_labels_rh = mne.stc_to_label(stc_mean_label,
-                                          src=src,
-                                          smooth=True,
-                                          subjects_dir=subjects_dir,
-                                          connected=True)
+    _, func_labels_rh = mne.stc_to_label(stc_mean_label,
+                                         src=src,
+                                         smooth=True,
+                                         subjects_dir=subjects_dir,
+                                         connected=True)
     # take first as func_labels are ordered based on maximum values in stc
-    func_label_rh = func_labels_rh[0]  
+    func_label_rh = func_labels_rh[0]
 
-    
-    func_label_lh.save(mne_folder + "%s-func_label_cond-%s_lh" 
+    func_label_lh.save(mne_folder + "%s-func_label_cond-%s_lh"
                        % (subject, side))
     func_label_rh.save(mne_folder + "%s-func_label_cond-%s_rh"
                        % (subject, side))
-                           
+
 #        return func_label_lh, func_label_rh
