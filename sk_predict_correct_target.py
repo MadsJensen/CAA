ea6113e1d54276f0fb025c964f06a889dfcf9901
Mads Jensen <mje.mads@gmail.com>
2016-04-29 10:21:11 +0200

    initial commit, NOT done

---
+++ sk_predict_correct_target.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from my_settings import *
+
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.cross_validation import StratifiedShuffleSplit, cross_val_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+data = pd.read_csv(data_path + "alpha_mean_pow_data_extracted_phase_target.csv")
+data = data.drop("mean", 1)
+data["corr"], corr_lbl = pd.factorize(data.correct)
+
+data_dv = pd.get_dummies(data[["ROI", "condition_side",
+                               "condition_type", "phase"]])
+data_dv["pow"] = data.power
+
+y = data["corr"].get_values()
+X = data_dv.get_values()
+
+cv = StratifiedShuffleSplit(y, n_iter=10)
+ada_params = {"n_estimators": np.arange(1, 11, 1),
+              "learning_rate": np.arange(0.01, 1, 0.1)}
+
+ada_grid = GridSearchCV(AdaBoostClassifier(), param_grid=ada_params, cv=cv,
+                        scoring="roc_auc")
+ada_grid.fit(X, y)
+
+
+data_itc = pd.read_csv(data_path + "alpha_mean_itc_data_extracted_phase_target.csv")
+data_itc = data_itc.drop("mean", 1)
+
+data_dv["itc"] = data_itc["itc"]
+
+y = data["corr"].get_values()
+X = data_dv.get_values()
+
+
+
+
+cv = StratifiedShuffleSplit(y, n_iter=10)
+ada_params = {"adaboostclassifier__n_estimators": np.arange(1, 50, 1),
+              "adaboostclassifier__learning_rate": np.arange(0.01, 1, 0.1)}
+
+scaler_pipe = make_pipeline(StandardScaler(), AdaBoostClassifier())
+grid = GridSearchCV(scaler_pipe, param_grid=ada_params, cv=cv)
+
+
+ada_grid = GridSearchCV(AdaBoostClassifier(), param_grid=ada_params, cv=cv,
+                        scoring="roc_auc")
+ada_grid.fit(X, y)
+
+ada = ada_grid.best_estimator_
+
+scores = cross_val_score(ada, X, y, cv=cv, scoring="roc_auc")
