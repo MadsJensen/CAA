a462dcb98e9d83f989531dc0c84a3990bf66554b
Mads Jensen <mje.mads@gmail.com>
2016-04-29 10:44:49 +0200

    Initial commit, NOt sure it is working

---
+++ sk_predict_correct_target_GBC.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from my_settings import *
+
+from sklearn.ensemble import GradientBoostingClassifier 
+from sklearn.cross_validation import StratifiedShuffleSplit, cross_val_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+data = pd.read_csv(data_path +
+                   "alpha_mean_pow_data_extracted_phase_target.csv")
+data = data.drop("mean", 1)
+data["corr"], corr_lbl = pd.factorize(data.correct)
+
+data_dv = pd.get_dummies(data[["ROI", "condition_side",
+                               "condition_type", "phase"]])
+data_dv["pow"] = data.power
+
+data_itc = pd.read_csv(data_path +
+                       "alpha_mean_itc_data_extracted_phase_target.csv")
+data_itc = data_itc.drop("mean", 1)
+
+data_dv["itc"] = data_itc["itc"]
+
+y = data["corr"].get_values()
+X = data_dv.get_values()
+
+
+cv = StratifiedShuffleSplit(y, n_iter=10)
+ada_params = {"gradientboostingclassifier__n_estimators": np.arange(1, 50, 1),
+              "gradientboostingclassifier__learning_rate":
+              np.arange(0.01, 1, 0.2),
+              "gradientboostingclassifier__max_depth": np.araange(1, 8, 2)}
+
+scaler_pipe = make_pipeline(StandardScaler(), GradientBoostingClassifier())
+grid = GridSearchCV(scaler_pipe, param_grid=ada_params, cv=cv)
+
+ada_grid.fit(X, y)
+
+ada = ada_grid.best_estimator_
+
+scores = cross_val_score(ada, X, y, cv=cv, scoring="roc_auc")
