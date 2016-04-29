1a619f0db0d41ce7f4ef0030c18e471b24e5f412
Mads Jensen <mje.mads@gmail.com>
2016-04-29 10:32:30 +0200

    changes

--- sk_predict_correct_target.py
+++ sk_predict_correct_target.py
@@ -8,7 +8,8 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
-data = pd.read_csv(data_path + "alpha_mean_pow_data_extracted_phase_target.csv")
+data = pd.read_csv(data_path +
+                   "alpha_mean_pow_data_extracted_phase_target.csv")
 data = data.drop("mean", 1)
 data["corr"], corr_lbl = pd.factorize(data.correct)
 
@@ -16,19 +17,8 @@ data_dv = pd.get_dummies(data[["ROI", "condition_side",
                                "condition_type", "phase"]])
 data_dv["pow"] = data.power
 
-y = data["corr"].get_values()
-X = data_dv.get_values()
-
-cv = StratifiedShuffleSplit(y, n_iter=10)
-ada_params = {"n_estimators": np.arange(1, 11, 1),
-              "learning_rate": np.arange(0.01, 1, 0.1)}
-
-ada_grid = GridSearchCV(AdaBoostClassifier(), param_grid=ada_params, cv=cv,
-                        scoring="roc_auc")
-ada_grid.fit(X, y)
-
-
-data_itc = pd.read_csv(data_path + "alpha_mean_itc_data_extracted_phase_target.csv")
+data_itc = pd.read_csv(data_path +
+                       "alpha_mean_itc_data_extracted_phase_target.csv")
 data_itc = data_itc.drop("mean", 1)
 
 data_dv["itc"] = data_itc["itc"]
@@ -37,12 +27,11 @@ y = data["corr"].get_values()
 X = data_dv.get_values()
 
 
-
-
 cv = StratifiedShuffleSplit(y, n_iter=10)
 ada_params = {"adaboostclassifier__n_estimators": np.arange(1, 50, 1),
               "adaboostclassifier__learning_rate": np.arange(0.01, 1, 0.1)}
 
+ada = AdaBoostClassifier
 scaler_pipe = make_pipeline(StandardScaler(), AdaBoostClassifier())
 grid = GridSearchCV(scaler_pipe, param_grid=ada_params, cv=cv)
 
