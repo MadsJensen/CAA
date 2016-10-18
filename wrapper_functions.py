8a41bc564a7b14eb675b02d18f0840f2936972d1
Mads Jensen <au194693@d17404.local>
2016-10-18 22:08:20 +0200

    Updated to use new submit_to_cluster function

--- wrapper_functions.py
+++ wrapper_functions.py
@@ -8,7 +8,7 @@ Doc string here.
 import sys
 import subprocess
 
-cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
+# cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
 
 subjects_select = ["0005", "0006", "0007", "0008", "0009", "0010",
                    "0011", "0015", "0016", "0017", "0020", "0021",
@@ -17,9 +17,9 @@ subjects_select = ["0005", "0006", "0007", "0008", "0009", "0010",
 if len(sys.argv) == 3:
     cpu_number = sys.argv[2]
 else:
-    cpu_number = 4
+    cpu_number = 1
 
 
 for subject in subjects_select:
-    submit_cmd = "python %s %s" % (sys.argv[1], subject)
-    subprocess.call([cmd, "%s" % cpu_number, submit_cmd])
+    submit_cmd = 'submit_to_cluster "python %s %s"' % (sys.argv[1], subject)
+    subprocess.call([submit_cmd])
