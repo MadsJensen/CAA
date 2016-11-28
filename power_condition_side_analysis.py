595cdd6321aba6c7d423b617eb99622e4edc6778
Mads Jensen <mje.mads@gmail.com>
2016-11-28 16:02:17 +0100

    fix syntax

--- power_condition_side_analysis.py
+++ power_condition_side_analysis.py
@@ -3,7 +3,11 @@ import numpy as np
 import mne
 import pandas as pd
 
-from my_settings import (epochs_folder, tf_folder, subjects_select)
+from my_settings import (epochs_folder, tf_folder)
+
+subjects_select = ["0005", "0006", "0007", "0008", "0009", "0010",
+                   "0011", "0015", "0016", "0017", "0020", "0021",
+                   "0022", "0024", "0025"]
 
 epochs = mne.read_epochs(
     epochs_folder + "0005_target-epo.fif", preload=False)
@@ -17,16 +21,16 @@ from_time = np.abs(times + 0.08).argmin()
 to_time = np.abs(times - 0.02).argmin()
 
 
-data_all = pd.DataFrame()
+df = pd.DataFrame()
 
 for subject in subjects_select:
     for condition in conditions:
         for side in sides:
             for roi in ROIS:
-                dat = np.load("%s_pow_%s_side_dSPM_Brodmann.17-%s_start.npy" %
+                dat = np.load(tf_folder + "%s_pow_%s_%s_dSPM_Brodmann.17-%s_target.npy" %
                               (subject, condition, side, roi))
 
-                value = dat[:, :, from_time:to_time].mean(axis=0).mean(axis=0)
+                value = dat[:, :, from_time:to_time].mean(axis=0).mean(axis=0).mean(axis=0)
                 
                 row = pd.DataFrame([{"subject": subject,
                                      "condition": condition,
