08ca7e770f2788ce7d58a9b0a72ab30873a77a8d
Mads Jensen <mads@cnru.dk>
2016-11-29 08:51:53 +0100

    Syntax fixes

--- power_condition_side_analysis.py
+++ power_condition_side_analysis.py
@@ -7,12 +7,12 @@ from my_settings import (epochs_folder, tf_folder, result_dir)
 
 method = "MNE"
 
-subjects_select = ["0005", "0006", "0007", "0008", "0009", "0010",
-                   "0011", "0015", "0016", "0017", "0020", "0021",
-                   "0022", "0024", "0025"]
+subjects_select = [
+    "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0015", "0016",
+    "0017", "0020", "0021", "0022", "0024", "0025"
+]
 
-epochs = mne.read_epochs(
-    epochs_folder + "0005_target-epo.fif", preload=False)
+epochs = mne.read_epochs(epochs_folder + "0005_target-epo.fif", preload=False)
 times = epochs.times
 
 conditions = ["ctl", "ent"]
@@ -22,26 +22,28 @@ ROIS = ["rh", "lh"]
 from_time = np.abs(times + 0.08).argmin()
 to_time = np.abs(times - 0.02).argmin()
 
-
 df = pd.DataFrame()
 
 for subject in subjects_select:
     for condition in conditions:
         for side in sides:
             for roi in ROIS:
-                dat = np.load(tf_folder + "%s_pow_%s_%s_%s_Brodmann.17-%s_target.npy" %
-                              (subject, condition, side, method, roi))
+                dat = np.load(tf_folder +
+                              "%s_pow_%s_%s_%s_Brodmann.17-%s_target.npy" % (
+                                  subject, condition, side, method, roi))
 
-                value = dat[:, :, from_time:to_time].mean(axis=0).mean(axis=0).mean(axis=0)
-                
-                row = pd.DataFrame([{"subject": subject,
-                                     "condition": condition,
-                                     "side": side,
-                                     "roi": roi,
-                                     "power": value}])
-                                    
+                value = dat[:, :, from_time:to_time].mean(axis=0).mean(
+                    axis=0).mean(axis=0)
+
+                row = pd.DataFrame([{
+                    "subject": subject,
+                    "condition": condition,
+                    "side": side,
+                    "roi": roi,
+                    "power": value
+                }])
+
                 df = df.append(row, ignore_index=True)
 
-
-
-df.to_csv(result_dir + "power_condition_side_mean_%s.csv" % method, index=False)
+df.to_csv(
+    result_dir + "power_condition_side_mean_%s.csv" % method, index=False)
