b51ef08b366835604b53444e1a116dd30e5519a0
Mads Jensen <mje.mads@gmail.com>
2017-03-12 22:02:52 +0100

    along the way

--- time_decoding_subject.py
+++ time_decoding_subject.py
@@ -2,7 +2,6 @@ import sys
 import numpy as np
 import mne
 from mne.decoding import GeneralizationAcrossTime
-from mne.time_frequency import read_tfrs
 from sklearn.externals import joblib
 
 from my_settings import (tf_folder, data_path, epochs_folder)
@@ -13,22 +12,22 @@ matplotlib.use('Agg')
 subject = sys.argv[1]
 
 # Load epochs from both conditions
-tfrs_ctl_left = read_tfrs(tf_folder + "%s_ctl_left-4-tfr.hd5" % (subject))
-tfrs_ctl_right = read_tfrs(tf_folder + "%s_ctl_right-4-tfr.hd5" % (subject))
+data_ctl_left = np.load(tf_folder + "%s_ctl_left-4-tfr.npy" % (subject))
+data_ctl_right = np.load(tf_folder + "%s_ctl_right-4-tfr.npy" % (subject))
 epochs = mne.read_epochs(
-    epochs_folder + "%s_trial_start-epo.fif" % subject, preload=False)
+    epochs_folder + "%s_trial_start-epo.fif" % subject, preload=True)
+epochs.resample(250)
 
 # Fix the events for the plan epochs so they can be concatenated
-epochs_ctl_left = mne.EpochsArray(tfrs_ctl_left.data.mean(axis=2), epochs.info)
-epochs_ctl_right = mne.EpochsArray(
-    tfrs_ctl_right.data.mean(axis=2), epochs.info)
+epochs_ctl_left = mne.EpochsArray(np.mean(np.abs(data_ctl_left)**2,axis=2),
+                                  epochs.info)
+epochs_ctl_right = mne.EpochsArray(np.mean(np.abs(data_ctl_right)**2, axis=2),
+                                   epochs.info)
 
 # Equalise channels and epochs, and concatenate epochs
 mne.equalize_channels([epochs_ctl_left, epochs_ctl_right])
 mne.epochs.equalize_epoch_counts([epochs_ctl_left, epochs_ctl_right])
 
-# Dirty hack # TODO: Check this from the Maxfilter side
-# epochs_classic.info['dev_head_t'] = epochs_plan.info['dev_head_t']
 
 epochs_tfr = mne.concatenate_epochs([epochs_ctl_left, epochs_ctl_right])
 
