efef81e6614f8103e61c18cb90266281d3ad5027
Mads Jensen <mads@cnru.dk>
2017-03-14 08:33:14 +0100

    Updated for all conditions

--- time_decoding_subject.py
+++ time_decoding_subject.py
@@ -13,17 +13,21 @@ subject = sys.argv[1]
 
 # Load epochs from both conditions
 data_ctl_left = np.load(tf_folder + "%s_ctl_left-4-tfr.npy" % (subject))
-data_ctl_right = np.load(tf_folder + "%s_ctl_right-4-tfr.npy" % (subject))
+data_ent_left = np.load(tf_folder + "%s_ctl_left-4-tfr.npy" % (subject))
+data_ctl_right = np.load(tf_folder + "%s_ent_right-4-tfr.npy" % (subject))
+data_ent_right = np.load(tf_folder + "%s_ent_right-4-tfr.npy" % (subject))
 epochs = mne.read_epochs(
     epochs_folder + "%s_trial_start-epo.fif" % subject, preload=True)
 epochs.resample(250)
 
 X = np.vstack([
-    np.mean(data_ctl_left, axis=2),
-    np.mean(data_ctl_right, axis=2)
+    np.mean(data_ctl_left, axis=2), np.mean(data_ctl_right, axis=2),
+    np.mean(data_ent_left, axis=2), np.mean(data_ent_right, axis=2)
 ])
-y = np.concatenate(
-    [np.zeros(len(data_ctl_left)), np.ones(len(data_ctl_right))])
+y = np.concatenate([
+    np.zeros(len(data_ctl_left)), np.ones(len(data_ctl_right)),
+    np.ones(len(data_ent_right)) * 2, np.ones(len(data_ent_right)) * 3
+])
 
 # Create epochs to use for classification
 n_trial, n_chan, n_time = X.shape
@@ -55,8 +59,7 @@ gat.score(epochs_data, y=y)
 joblib.dump(gat, data_path + "decode_time_gen/%s_gat_2.jl" % subject)
 
 # make matrix plot and save it
-fig = gat.plot(
-    cmap="viridis", title="Temporal Gen for subject: %s" % subject)
+fig = gat.plot(cmap="viridis", title="Temporal Gen for subject: %s" % subject)
 fig.savefig(data_path + "decode_time_gen/%s_gat_matrix.png" % subject)
 
 fig = gat.plot_diagonal(
