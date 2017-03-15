d84c1a14fd554aee0aa40c6e9942c98a76360f3d
Mads Jensen <mads@cnru.dk>
2017-03-15 13:08:52 +0100

    fixes

--- time_decoding_all-subjects.py
+++ time_decoding_all-subjects.py
@@ -1,6 +1,6 @@
 import numpy as np
 import mne
-from mne import create_info, EpochsArray
+from mne import create_info
 from mne.decoding import GeneralizationAcrossTime
 
 from sklearn.externals import joblib
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
-from my_settings import (tf_folder, data_path, subjects)
+from my_settings import (tf_folder, data_path, subjects_select)
 
 import matplotlib
 matplotlib.use('Agg')
@@ -18,7 +18,8 @@ ave_ctl_right = []
 ave_ent_left = []
 ave_ent_right = []
 
-for subject in subjects:
+for subject in subjects_select:
+    print("loading subject: %s" % subject)
     epochs = mne.read_epochs(tf_folder + "%s_power-epo.fif" % subject)
     # Crop and downsmample to make it faster
     epochs.crop(tmin=-0.2, tmax=1)
@@ -33,12 +34,12 @@ for subject in subjects:
     ave_ent_left.append(epochs["ent/left"].average())
     ave_ent_right.append(epochs["ent/right"].average())
 
-
 ave_ctl_left = np.asarray(ave_ctl_left)
 ave_ctl_right = np.asarray(ave_ctl_right)
 ave_ent_left = np.asarray(ave_ent_left)
 ave_ent_right = np.asarray(ave_ent_right)
 
+print("making X & y")
 X = np.vstack([ave_ctl_left, ave_ctl_right, ave_ent_left, ave_ent_right])
 y = np.concatenate([
     np.zeros(len(ave_ctl_left)), np.ones(len(ave_ctl_right)),
@@ -52,6 +53,8 @@ chan_names = ['MEG %i' % chan for chan in range(n_chan)]
 chan_types = ['mag'] * n_chan
 sfreq = 250
 info = create_info(chan_names, sfreq, chan_types)
+
+print("Creating epochs")
 epochs_data = mne.EpochsArray(data=X, info=info, events=events, verbose=False)
 epochs_data.times = epochs.times[::4][:-1]
 
@@ -73,13 +76,16 @@ print("Scoring GAT")
 gat.score(epochs_data)
 
 # Save model
-joblib.dump(gat, data_path + "decode_time_gen/%s_gat_all_grad.jl" % subject)
+print("Saving model")
+joblib.dump(gat,
+            data_path + "decode_time_gen/%s_gat_all-subs_grad.jl" % subject)
 
 # make matrix plot and save it
 fig = gat.plot(cmap="viridis", title="Temporal Gen for subject: %s" % subject)
-fig.savefig(data_path + "decode_time_gen/%s_gat_matrix_all_grad.png" % subject)
+fig.savefig(data_path + "decode_time_gen/%s_gat_matrix_all-subs_grad.png" %
+            subject)
 
 fig = gat.plot_diagonal(
     chance=0.5, title="Temporal Gen for subject: %s" % subject)
-fig.savefig(data_path + "decode_time_gen/%s_gat_diagonal_all_grad.png" %
+fig.savefig(data_path + "decode_time_gen/%s_gat_diagonal_all-subs_grad.png" %
             subject)
