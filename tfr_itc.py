576b2245fbbb7ab423b35c3e080e9fdc5ea56abc
Mads Jensen <mads@cnru.dk>
2017-03-30 13:47:09 +0200

    Inital itc calc commit

---
+++ tfr_itc.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 09:14:59 2017
+
+@author: au194693
+"""
+
+import mne
+import numpy as np
+from my_settings import (epochs_folder, tf_folder)
+from mne.time_frequency import tfr_morlet
+import sys
+
+subject = sys.argv[1]
+
+freqs = np.arange(8, 13, 1)  # define frequencies of interest
+n_cycles = 4.  # freqs / 2.  # different number of cycle per frequency
+
+sides = ["left", "right"]
+conditions = ["ctl", "ent"]
+
+epochs = mne.read_epochs(
+    epochs_folder + "%s_trial_start-epo.fif" % subject, preload=True)
+epochs.resample(250)
+
+for cond in conditions:
+    for side in sides:
+        power, itc = tfr_morlet(
+            epochs[cond + "/" + side],
+            freqs=freqs,
+            n_cycles=n_cycles,
+            use_fft=True,
+            average=True,
+            return_itc=True,
+            n_jobs=1)
+        np.save(tf_folder + "%s_%s_%s-4-itc.npy" % (subject, cond, side),
+                itc.data)
