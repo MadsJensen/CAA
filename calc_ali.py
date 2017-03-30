c691a5b7c4a9838b18a92e098c91c31396853b7a
Mads Jensen <mads@cnru.dk>
2017-03-30 10:52:34 +0200

    along the way

--- calc_ali.py
+++ calc_ali.py
@@ -6,7 +6,7 @@ This is a group of function to be used on TF data.
 @email: mads [] cnru.dk
 """
 
-from my_settings import (epochs_folder)
+from my_settings import (epochs_folder, tf_folder)
 import numpy as np
 import mne
 import sys
@@ -21,8 +21,14 @@ def find_channel_index(subject, selection=selection_name):
         epochs_folder + "%s_trial_start-epo.fif" % subject, preload=False)
     selection = mne.read_selection(selection_name)
     selection = [f.split()[0] + f.split()[1] for f in selection]
-    picks = mne.pick_types(epochs.info, meg='grad', eeg=False, eog=False,
-                           stim=False, exclude=[], selection=selection)
+    picks = mne.pick_types(
+        epochs.info,
+        meg='grad',
+        eeg=False,
+        eog=False,
+        stim=False,
+        exclude=[],
+        selection=selection)
 
     return picks
 
@@ -45,22 +51,10 @@ def calc_ALI(subject, show_plot=False):
     ali_left : the ALI for the left cue
     ali_right : the ALI for the right cue
     """
-    ctl_left_roi_left_cue =\
-        mne.read_source_estimate(tf_folder +
-                                 "BP_%s_ctl_left_OCCIPITAL_lh_dSPM"
-                                 % (subject))
-    ctl_right_roi_left_cue =\
-        mne.read_source_estimate(tf_folder +
-                                 "BP_%s_ctl_left_OCCIPITAL_rh_dSPM"
-                                 % (subject))
-    ctl_left_roi_right_cue =\
-        mne.read_source_estimate(tf_folder +
-                                 "BP_%s_ctl_right_OCCIPITAL_lh_dSPM"
-                                 % (subject))
-    ctl_right_roi_right_cue =\
-        mne.read_source_estimate(tf_folder +
-                                 "BP_%s_ctl_right_OCCIPITAL_rh_dSPM"
-                                 % (subject))
+    ctl_left = np.load(tf_folder + "%s_ctl_left-4-tfr.npy" % (subject))
+    ctl_right = np.load(tf_folder + "%s_ctl_right-4-tfr.npy" % (subject))
+    ent_left = np.load(tf_folder + "%s_ent_left-4-tfr.npy" % (subject))
+    ent_right = np.load(tf_folder + "%s_ent_right-4-tfr.npy" % (subject))
 
     ALI_left_cue_ctl = ((ctl_left_roi_left_cue.data.mean(axis=0) -
                          ctl_right_roi_left_cue.data.mean(axis=0)) /
@@ -110,7 +104,5 @@ def calc_ALI(subject, show_plot=False):
         plt.title("ALI curves for subject: %s" % subject)
         plt.show()
 
-    return (ALI_left_cue_ctl, ALI_right_cue_ctl,
-            ALI_left_cue_ent, ALI_right_cue_ent)
-
-
+    return (ALI_left_cue_ctl, ALI_right_cue_ctl, ALI_left_cue_ent,
+            ALI_right_cue_ent)
