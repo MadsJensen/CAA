c2b19ccd232376e285ebc9873c7817ebe239f469
Mads Jensen <mads@cnru.dk>
2017-03-31 17:19:42 +0200

    PEP8

--- ali_stats_sensor_grads.py
+++ ali_stats_sensor_grads.py
@@ -8,10 +8,9 @@ Created on Thu Mar 30 13:14:24 2017
 import matplotlib.pyplot as plt
 import numpy as np
 from mne.stats import permutation_cluster_test
-import mne 
+import mne
 
-from my_settings import (subjects_select,tf_folder, epochs_folder)
-
+from my_settings import (subjects_select, tf_folder, epochs_folder)
 
 ctl_left = []
 ctl_right = []
@@ -32,83 +31,96 @@ ent_right = np.asarray(ent_right)
 
 epochs = mne.read_epochs(
     epochs_folder + "0005_trial_start-epo.fif", preload=False)
-times = (epochs.times[::4][:-1])*1e3
+times = (epochs.times[::4][:-1]) * 1e3
 
-T_obs, clusters, cluster_pv, H0 = permutation_cluster_test([ctl_left, ctl_right],
-                                                           n_permutations=5000)
+T_obs, clusters, cluster_pv, H0 = permutation_cluster_test(
+    [ctl_left, ctl_right], n_permutations=5000)
 
 plt.figure()
 plt.close('all')
 plt.subplot(211)
 plt.title("Ctl left v right")
-plt.plot(times, ctl_left.mean(axis=0) - ctl_right.mean(axis=0),
-         label="ERF Contrast (Event 1 - Event 2)")
+plt.plot(
+    times,
+    ctl_left.mean(axis=0) - ctl_right.mean(axis=0),
+    label="ERF Contrast (Event 1 - Event 2)")
 plt.ylabel("MEG (T / m)")
 plt.legend()
 plt.subplot(212)
 for i_c, c in enumerate(clusters):
     c = c[0]
     if cluster_pv[i_c] <= 0.05:
-        h = plt.axvspan(times[c.start], times[c.stop - 1],
-                        color='r', alpha=0.3)
+        h = plt.axvspan(
+            times[c.start], times[c.stop - 1], color='r', alpha=0.3)
     else:
-        plt.axvspan(times[c.start], times[c.stop - 1], color=(0.3, 0.3, 0.3),
-                    alpha=0.3)
+        plt.axvspan(
+            times[c.start],
+            times[c.stop - 1],
+            color=(0.3, 0.3, 0.3),
+            alpha=0.3)
 hf = plt.plot(times, T_obs, 'g')
 plt.legend((h, ), ('cluster p-value < 0.05', ))
 plt.xlabel("time (ms)")
 plt.ylabel("f-values")
 plt.show()
-
 
 d_ctl_ali = ctl_left - ctl_right
 d_ent_ali = ent_left - ent_right
 
-T_obs, clusters, cluster_pv, H0 = permutation_cluster_test([d_ctl_ali, d_ent_ali],
-                                                           n_permutations=5000)
+T_obs, clusters, cluster_pv, H0 = permutation_cluster_test(
+    [d_ctl_ali, d_ent_ali], n_permutations=5000)
 
 plt.figure()
 plt.subplot(211)
 plt.title("Ctl v ent ")
-plt.plot(times, d_ctl_ali.mean(axis=0) - d_ent_ali.mean(axis=0),
-         label="ERF Contrast (Event 1 - Event 2)")
+plt.plot(
+    times,
+    d_ctl_ali.mean(axis=0) - d_ent_ali.mean(axis=0),
+    label="ERF Contrast (Event 1 - Event 2)")
 plt.ylabel("MEG (T / m)")
 plt.legend()
 plt.subplot(212)
 for i_c, c in enumerate(clusters):
     c = c[0]
     if cluster_pv[i_c] <= 0.05:
-        h = plt.axvspan(times[c.start], times[c.stop - 1],
-                        color='r', alpha=0.3)
+        h = plt.axvspan(
+            times[c.start], times[c.stop - 1], color='r', alpha=0.3)
     else:
-        plt.axvspan(times[c.start], times[c.stop - 1], color=(0.3, 0.3, 0.3),
-                    alpha=0.3)
+        plt.axvspan(
+            times[c.start],
+            times[c.stop - 1],
+            color=(0.3, 0.3, 0.3),
+            alpha=0.3)
 hf = plt.plot(times, T_obs, 'g')
 plt.legend((h, ), ('cluster p-value < 0.05', ))
 plt.xlabel("time (ms)")
 plt.ylabel("f-values")
 plt.show()
 
-
-T_obs, clusters, cluster_pv, H0 = permutation_cluster_test([ent_left, ent_right],
-                                                           n_permutations=5000)
+T_obs, clusters, cluster_pv, H0 = permutation_cluster_test(
+    [ent_left, ent_right], n_permutations=5000)
 
 plt.figure()
 plt.subplot(211)
 plt.title("Ent left v right")
-plt.plot(times, d_ctl_ali.mean(axis=0) - d_ent_ali.mean(axis=0),
-         label="ERF Contrast (Event 1 - Event 2)")
+plt.plot(
+    times,
+    d_ctl_ali.mean(axis=0) - d_ent_ali.mean(axis=0),
+    label="ERF Contrast (Event 1 - Event 2)")
 plt.ylabel("MEG (T / m)")
 plt.legend()
 plt.subplot(212)
 for i_c, c in enumerate(clusters):
     c = c[0]
     if cluster_pv[i_c] <= 0.05:
-        h = plt.axvspan(times[c.start], times[c.stop - 1],
-                        color='r', alpha=0.3)
+        h = plt.axvspan(
+            times[c.start], times[c.stop - 1], color='r', alpha=0.3)
     else:
-        plt.axvspan(times[c.start], times[c.stop - 1], color=(0.3, 0.3, 0.3),
-                    alpha=0.3)
+        plt.axvspan(
+            times[c.start],
+            times[c.stop - 1],
+            color=(0.3, 0.3, 0.3),
+            alpha=0.3)
 hf = plt.plot(times, T_obs, 'g')
 plt.legend((h, ), ('cluster p-value < 0.05', ))
 plt.xlabel("time (ms)")
