f230a8803f064ccee435332dadb4bc06d677c90a
Mads Jensen <mads@cnru.dk>
2017-03-31 11:13:41 +0200

    Add ctl condition

--- calc_itc_ali.py
+++ calc_itc_ali.py
@@ -16,8 +16,11 @@ from my_settings import (subjects_select, tf_folder, epochs_folder)
 d_ali_ent_right = []
 
 for subject in subjects_select:
-    data_right = np.load(tf_folder + "%s_ent_right-4-itc.npy" % subject)
-    data_left = np.load(tf_folder + "%s_ent_left-4-itc.npy" % subject)
+    data_right_ent = np.load(tf_folder + "%s_ent_right-4-itc.npy" % subject)
+    data_left_ent = np.load(tf_folder + "%s_ent_left-4-itc.npy" % subject)
+
+    data_right_ctl = np.load(tf_folder + "%s_ctl_right-4-itc.npy" % subject)
+    data_left_ctl = np.load(tf_folder + "%s_ctl_left-4-itc.npy" % subject)
 
     epochs = mne.read_epochs(
         epochs_folder + "%s_trial_start-epo.fif" % subject, preload=False)
@@ -43,16 +46,25 @@ for subject in subjects_select:
         exclude=[],
         selection=selection)
 
-    d_right = (data_left[right_idx, :, :] - data_right[right_idx, :, :])
-    d_left = (data_left[left_idx, :, :] - data_right[left_idx, :, :])
+    d_right_ent = (
+        data_left_ent[right_idx, :, :] - data_right_ent[right_idx, :, :])
+    d_left_ent = (
+        data_left_ent[left_idx, :, :] - data_right_ent[left_idx, :, :])
 
-d_ali_ent_right = np.asarray(d_right).mean(axis=1)
-d_ali_ent_left = np.asarray(d_left).mean(axis=1)
+    d_right_ctl = (
+        data_left_ctl[right_idx, :, :] - data_right_ctl[right_idx, :, :])
+    d_left_ctl = (
+        data_left_ctl[left_idx, :, :] - data_right_ctl[left_idx, :, :])
+
+d_ali_ent_right = np.asarray(d_right_ent).mean(axis=1)
+d_ali_ent_left = np.asarray(d_left_ent).mean(axis=1)
+d_ali_ctl_right = np.asarray(d_right_ctl).mean(axis=1)
+d_ali_ctl_left = np.asarray(d_left_ctl).mean(axis=1)
 
 T_obs, clusters, cluster_pv, H0 = permutation_cluster_test(
     [d_ali_ent_left, d_ali_ent_right], n_permutations=5000)
 
-times = (epochs.times[::4][:-1])*1e3
+times = (epochs.times[::4][:-1]) * 1e3
 plt.close('all')
 plt.subplot(211)
 plt.title("Ctl left v right")
