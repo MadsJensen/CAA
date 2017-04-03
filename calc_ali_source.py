8b9335cdc05e9f66faf779c2faeeda969e017cc5
Mads Jensen <mads@cnru.dk>
2017-04-03 11:50:23 +0200

    Squeeze fix

--- calc_ali_source.py
+++ calc_ali_source.py
@@ -119,31 +119,33 @@ def calc_ALI_itc(subject):
     ali_left : the ALI for the left cue
     ali_right : the ALI for the right cue
     """
-    ctl_right_rh = np.load(tf_folder +
-                           "%s_ctl_right_Brodmann.17-rh_dSPM_pca-ts.npy" %
-                           (subject)).squzee()
-    ctl_right_lh = np.load(tf_folder +
-                           "%s_ctl_right_Brodmann.17-lh_dSPM_pca-ts.npy" %
-                           (subject)).squzee()
-    ctl_left_rh = np.load(tf_folder +
-                          "%s_ctl_left_Brodmann.17-rh_dSPM_pca-ts.npy" %
-                          (subject)).squzee()
-    ctl_left_lh = np.load(tf_folder +
-                          "%s_ctl_left_Brodmann.17-lh_dSPM_pca-ts.npy" %
-                          (subject)).squzee()
+    ctl_right_rh = np.load(
+        tf_folder + "%s_ctl_right_Brodmann.17-rh_dSPM_pca-ts.npy" % (subject))
+    ctl_right_lh = np.load(
+        tf_folder + "%s_ctl_right_Brodmann.17-lh_dSPM_pca-ts.npy" % (subject))
+    ctl_left_rh = np.load(
+        tf_folder + "%s_ctl_left_Brodmann.17-rh_dSPM_pca-ts.npy" % (subject))
+    ctl_left_lh = np.load(
+        tf_folder + "%s_ctl_left_Brodmann.17-lh_dSPM_pca-ts.npy" % (subject))
 
-    ent_right_rh = np.load(tf_folder +
-                           "%s_ent_right_Brodmann.17-rh_dSPM_pca-ts.npy" %
-                           (subject)).squzee()
-    ent_right_lh = np.load(tf_folder +
-                           "%s_ent_right_Brodmann.17-lh_dSPM_pca-ts.npy" %
-                           (subject)).squzee()
-    ent_left_rh = np.load(tf_folder +
-                          "%s_ent_left_Brodmann.17-rh_dSPM_pca-ts.npy" %
-                          (subject)).squzee()
-    ent_left_lh = np.load(tf_folder +
-                          "%s_ent_left_Brodmann.17-lh_dSPM_pca-ts.npy" %
-                          (subject)).squzee()
+    ent_right_rh = np.load(
+        tf_folder + "%s_ent_right_Brodmann.17-rh_dSPM_pca-ts.npy" % (subject))
+    ent_right_lh = np.load(
+        tf_folder + "%s_ent_right_Brodmann.17-lh_dSPM_pca-ts.npy" % (subject))
+    ent_left_rh = np.load(
+        tf_folder + "%s_ent_left_Brodmann.17-rh_dSPM_pca-ts.npy" % (subject))
+    ent_left_lh = np.load(
+        tf_folder + "%s_ent_left_Brodmann.17-lh_dSPM_pca-ts.npy" % (subject))
+
+    ctl_left_lh = np.squeeze(ctl_left_lh)
+    ctl_left_rh = np.squeeze(ctl_left_rh)
+    ctl_right_lh = np.squeeze(ctl_right_lh)
+    ctl_right_rh = np.squeeze(ctl_right_rh)
+
+    ent_left_lh = np.squeeze(ent_left_lh)
+    ent_left_rh = np.squeeze(ent_left_rh)
+    ent_right_lh = np.squeeze(ent_right_lh)
+    ent_right_rh = np.squeeze(ent_right_rh)
 
     ALI_left_cue_ctl = ((ctl_left_lh.mean(axis=0) - ctl_left_rh.mean(axis=0)) /
                         (ctl_left_lh.mean(axis=0) + ctl_left_rh.mean(axis=0)))
