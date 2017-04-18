1e1f6bdefd87131bbcf4798baba3054259b4d4d6
Mads Jensen <mje.mads@gmail.com>
2017-04-18 15:02:35 +0200

    updates to loop, NOT working

--- extract_mean_power_itc.py
+++ extract_mean_power_itc.py
@@ -7,7 +7,7 @@ Created on Tue Apr 18 13:10:13 2017
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import mne
 
 from my_settings import (subjects_select, tf_folder, epochs_folder)
@@ -23,23 +23,40 @@ to_time = np.abs(times - 0.7).argmin()
 
 mean_power_df = pd.DataFrame()
 
+conditions = ["ctl", "ent"]
+sides = ["lh", "rh"]
+targets = ["left", "right"]
+
 for subject in subjects_select:
+    for cond in conditions:
+        for side in sides:
+            for target in targets:
+                data = np.load(
+                    tf_folder +
+                    "%s_%_%_LOBE.OCCIPITAL-%s_MNE_source_power_snr_3.npy" %
+                    (subject, cond, target, side))
+
+                data = np.percentile(ctl_left_lh, 90, axis=0)
+                data = data
+
     data = np.load(tf_folder + "%s_ali_pow_source_Lobes.npy" % subject)
     ctl_left = data[0, from_time:to_time].mean()
     ctl_right = data[1, from_time:to_time].mean()
     ent_left = data[2, from_time:to_time].mean()
     ent_right = data[3, from_time:to_time].mean()
-    
-    tmp_df = pd.DataFrame([{"subject": subject,
-                            "ctl_left": ctl_left,
-                            "ctl_right": ctl_right,
-                            "ent_left": ent_left,
-                            "ent_right": ent_right}])
-    
+
+    tmp_df = pd.DataFrame([{
+        "subject": subject,
+        "ctl_left": ctl_left,
+        "ctl_right": ctl_right,
+        "ent_left": ent_left,
+        "ent_right": ent_right
+    }])
+
     mean_power_df = mean_power_df.append(tmp_df, ignore_index=True)
 
 mean_ali_long = pd.melt(mean_power_df, id_vars=["subject"])
-mean_ali_long.columns  = ['subject', 'variable', 'ALI']
+mean_ali_long.columns = ['subject', 'variable', 'ALI']
 
 mean_ali_long.to_csv(tf_folder + "mean_ali_long.csv", index=False)
 
@@ -52,16 +69,18 @@ for subject in subjects_select:
     ctl_right = data[1, from_time:to_time].mean()
     ent_left = data[2, from_time:to_time].mean()
     ent_right = data[3, from_time:to_time].mean()
-    
-    tmp_df = pd.DataFrame([{"subject": subject,
-                            "ctl_left": ctl_left,
-                            "ctl_right": ctl_right,
-                            "ent_left": ent_left,
-                            "ent_right": ent_right}])
-    
-    mean_itc_df = mean_itc_df.append(tmp_df, ignore_index=True)    
+
+    tmp_df = pd.DataFrame([{
+        "subject": subject,
+        "ctl_left": ctl_left,
+        "ctl_right": ctl_right,
+        "ent_left": ent_left,
+        "ent_right": ent_right
+    }])
+
+    mean_itc_df = mean_itc_df.append(tmp_df, ignore_index=True)
 
 mean_itc_long = pd.melt(mean_itc_df, id_vars=["subject"])
-mean_itc_long.columns  = ['subject', 'variable', 'ITC']
+mean_itc_long.columns = ['subject', 'variable', 'ITC']
 
 mean_itc_long.to_csv(tf_folder + "mean_itc_long.csv", index=False)
